1e21ef2d3a68cd7b3a805e7f7304c915f68e1318
ruaridhm <ruaridh.mcguckin@gmail.com>
2021-09-08 10:47:33 +0100

    switched from react-ga to gatsby-plugin-google-gtag dependencies for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,22 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-X5FRHZRG2S", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import SkillSet from "../components/Skills"
@@ -6,14 +6,8 @@ import Projects from "../components/Projects"
 import Contact from "../components/Contact/Contact"
 import BackgroundLayer from "../components/BackgroundLayer"
 import { Helmet } from "react-helmet"
-import ReactGA from "react-ga"
 
 const Home = () => {
-  useEffect(() => {
-    ReactGA.initialize("G-X5FRHZRG2S")
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }, [])
-
   const [filteredSkills, setFilteredSkills] = useState({
     HTML: false,
     CSS: false,
@@ -44,15 +38,6 @@ const Home = () => {
           name="keywords"
           content="ruaridh mcguckin, ruaridh, portfolio, developer, front-end, web developer,front end, web developer, ,frontend, web development, web app, web site development, web app development, freelance web developer, web developer portfolio, react developer, gatsby developer, react, gatsby, javascript developer, dublin, ireland, responsive web developer,  "
         />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-X5FRHZRG2S"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-X5FRHZRG2S');
-        </script>
       </Helmet>
       <Hero />
       <BackgroundLayer>
